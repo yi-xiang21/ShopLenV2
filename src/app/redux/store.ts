98e44e68b2e9fc334b7299f0898d258850b9024b
EchConDamDang <haunghia1512@gmail.com>
2026-07-30 11:11:34 +0700

    Update store.ts

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import authReducer from '@/pages/Login&Register/store/auth-slice';
-import wishlistReducer from '@/pages/User/whistlist/store/wishlist_slice';
-import cart from '@/pages/User/cart/store/cart_slice';
+import authReducer from '../../pages/Login&Register/store/auth-slice';
+import wishlistReducer from '../../pages/User/whistlist/store/wishlist_slice';
+import cart from '../../pages/User/cart/store/cart_slice';
 export const store = configureStore({
   reducer: {
     auth: authReducer,
