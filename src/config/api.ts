3b00439ceb0e257cb809fc77f1c15d1b013a03e5
loctran312 <lommlay@gmail.com>
2026-08-07 11:36:00 +0700

    Fix url deploy

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,8 +1,8 @@
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
-
 // const API_BASE_URL =
-//   import.meta.env.VITE_API_BASE_URL || 'https://be-shoplen.onrender.com';
+//   import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
+
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || 'https://be-shoplen.onrender.com';
 
 export const API_CONFIG = {
   BASE_URL: API_BASE_URL,
